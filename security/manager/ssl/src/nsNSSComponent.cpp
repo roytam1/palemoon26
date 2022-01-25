c3eba7a712b65b29f730885f9b4e9c937885f7ea
roytam1 <roytam@gmail.com>
2022-01-25 22:25:11 +0800

    NSS: add TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384 support, also fix ChaCha20-Poly1305 name in ssl3_cipherName[]

--- security/manager/ssl/src/nsNSSComponent.cpp
+++ security/manager/ssl/src/nsNSSComponent.cpp
@@ -835,6 +835,8 @@ static const CipherPref CipherPrefs[] = {
    TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256, true },
  { "security.ssl3.ecdhe_rsa_aes_256_gcm_sha384",
    TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384, true },
+ { "security.ssl3.ecdhe_ecdsa_aes_256_gcm_sha384",
+   TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384, true },
  { "security.ssl3.ecdhe_rsa_aes_128_gcm_sha256",
    TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256, true },
  { "security.ssl3.ecdhe_ecdsa_aes_128_gcm_sha256",
