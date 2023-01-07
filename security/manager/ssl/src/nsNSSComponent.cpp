fdac442b1a2794ae8835a149876ff2fbcfcb0f1c
roytam1 <roytam@gmail.com>
2023-01-07 11:54:53 +0800

    [network][security] remove Camellia-GCM since it is unfinished, and add TLS_RSA_WITH_AES_128_GCM_SHA256

--- security/manager/ssl/src/nsNSSComponent.cpp
+++ security/manager/ssl/src/nsNSSComponent.cpp
@@ -844,10 +844,8 @@ static const CipherPref CipherPrefs[] = {
  { "security.ssl3.ecdhe_rsa_aes_128_sha256",
    TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256, true },
    
- { "security.ssl3.ecdhe_rsa_camellia_128_gcm_sha256",
-   TLS_ECDHE_RSA_WITH_CAMELLIA_128_GCM_SHA256, true },
- { "security.ssl3.ecdhe_ecdsa_camellia_128_gcm_sha256",
-   TLS_ECDHE_ECDSA_WITH_CAMELLIA_128_GCM_SHA256, true },
+ { "security.ssl3.rsa_aes_128_gcm_sha256",
+   TLS_RSA_WITH_AES_128_GCM_SHA256, true },
 
  { "security.ssl3.ecdhe_ecdsa_aes_256_sha",
    TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA, true },
