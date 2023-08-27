12845422ad3a99d05f692dad09f1be845cc21799
roytam1 <roytam@gmail.com>
2023-08-27 21:58:42 +0800

    tremor: update to https://github.com/sezero/tremor/tree/91decb5f1b11a84a2157a6326d9135b22627024b
    since upstream(i.e. xiph) doesn't update it since 2018.
    
    locally added NOMINMAX guard as well.

--- media/libtremor/update.sh
+++ media/libtremor/update.sh
@@ -30,4 +30,5 @@ cp $1/ivorbiscodec.h ./include/tremor/ivorbiscodec.h
 cp $1/os.h ./lib/os.h
 cp $1/COPYING ./COPYING
 cp $1/README ./README
-patch -p3 < ./bug719612.patch
+
+echo "Remember to update the version details at README_MCP."
